48834eae65f7f3028b842dbeefd2bd9cfcd7c7f5
kirillmaiboroda <kirillmaiboroda@gmail.com>
2021-02-02 16:13:45 +0300

    fixed manager for login from google

--- registration/users/managers.py
+++ registration/users/managers.py
@@ -7,7 +7,7 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
@@ -18,7 +18,10 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
-    
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, **extra_fields)
+
     def create_superuser(self, email, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
--- registration/users/urls.py
+++ registration/users/urls.py
@@ -6,7 +6,7 @@ app_name = 'users'
 
 
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('accounts/login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('registration/', UserRegistrationView.as_view(), name='user_registration'),
     path('index/', AccountView.as_view(), name='index'),
